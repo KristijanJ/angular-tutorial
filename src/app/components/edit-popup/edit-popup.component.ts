ebe0d5334dff54725b534bc9bbea328f2a80539f
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2024-07-24 16:18:29 +0200

    forms

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -2,7 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 
@@ -15,11 +21,14 @@ import { ButtonModule } from 'primeng/button';
     FormsModule,
     RatingModule,
     ButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
   @Input() display: boolean = false;
   @Output() displayChange = new EventEmitter<boolean>();
 
@@ -33,8 +42,40 @@ export class EditPopupComponent {
     rating: 0,
   };
 
+  // CUSTOM FORM VALIDATOR
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /a/.test(control.value);
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    };
+  }
+
+  // FORM
+  productForm = this.formBuilder.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        // this.specialCharacterValidator(),
+      ],
+    ],
+    image: [''],
+    price: ['', [Validators.required]],
+    rating: [0],
+  });
+
   onConfirm() {
-    this.confirm.emit(this.product);
+    const { name, image, price, rating } = this.productForm.value;
+
+    this.confirm.emit({
+      name: name || '',
+      image: image || '',
+      price: price || '',
+      rating: rating || 0,
+    });
+
     this.display = false;
     this.displayChange.emit(this.display);
   }
@@ -42,5 +83,10 @@ export class EditPopupComponent {
   onCancel() {
     this.display = false;
     this.displayChange.emit(this.display);
+  }
+
+  ngOnChanges() {
+    // POPULATE THE FORM
+    this.productForm.patchValue(this.product);
   }
 }
