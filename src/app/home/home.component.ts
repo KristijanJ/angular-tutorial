ce39a9a2db6ddb84791a5f73dfc0205fcb695e00
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2024-07-24 13:25:34 +0200

    pagination

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/products.service';
 import { ProductComponent } from '../components/product/product.component';
 import { Product } from '../../types';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule, PaginatorState } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -15,16 +16,27 @@ export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
   products: Product[] = [];
+  totalRecords: number = 0;
+  rows: number = 5;
 
-  ngOnInit() {
+  fetchProducts(page: number, perPage: number) {
     this.productsService
       .getProducts('http://localhost:3000/clothes', {
-        page: 0,
-        perPage: 5,
+        page,
+        perPage,
       })
       .subscribe((products) => {
         this.products = products.items;
+        this.totalRecords = products.total;
       });
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.fetchProducts(event.page!, event.rows!);
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 
   onProductOutput(product: Product) {
